181b3475870414e546402f191b880600d65a0cc6
look516 <happycatcindy@gmail.com>
2026-05-05 08:23:30 +0900

    코드카타 5회차

---
+++ 코드카타/Programmers05.java
@@ -0,0 +1,9 @@
+class Programmmers05 {
+    public int programmer05(int num1, int num2) {
+        int answer = -1;
+        if (num1 == num2) {
+            answer = 1;
+        }
+        return answer;
+    }
+}
