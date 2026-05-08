ea3d83f12cd38fdecf0c8e7f89388a877311610e
look516 <happycatcindy@gmail.com>
2026-05-08 23:17:58 +0900

    코드카타 11회차

---
+++ 코드카타/Programmers11.java
@@ -0,0 +1,9 @@
+public class Programmers11 {
+    public String Programmers11(int num) {
+            String answer = "Odd";
+            if (num % 2 == 0) {
+                answer = "Even";
+            }
+            return answer;
+    }
+}
